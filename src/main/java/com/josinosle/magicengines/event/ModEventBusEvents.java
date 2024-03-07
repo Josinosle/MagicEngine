65692f1e3f633d02505d3f69a4c86518fc67778a
Josinosle <josephbridger3@gmail.com>
2024-03-07 13:03:45 +0000

    initial

---
+++ src/main/java/com/josinosle/magicengines/event/ModEventBusEvents.java
@@ -0,0 +1,21 @@
+package com.josinosle.magicengines.event;
+
+import com.josinosle.magicengines.init.MagicEngines;
+import com.josinosle.magicengines.init.ParticleInit;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import com.josinosle.magicengines.Particle.CastParticles;
+
+
+@Mod.EventBusSubscriber(modid = MagicEngines.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+
+public class ModEventBusEvents {
+    @SubscribeEvent
+
+    public static void registerParticleFactories(final RegisterParticleProvidersEvent event) {
+        Minecraft.getInstance().particleEngine.register(ParticleInit.CAST_PARTICLES.get(),
+                CastParticles.Provider::new);
+    }
+}
