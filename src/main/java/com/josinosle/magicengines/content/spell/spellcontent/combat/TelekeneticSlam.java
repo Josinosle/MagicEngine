13a741b64fe0ee02cb75ec87551452bc766b8640
Josinosle <josephbridger3@gmail.com>
2024-03-10 22:21:49 +0000

    networking fix

--- src/main/java/com/josinosle/magicengines/content/spell/spellcontent/combat/TelekeneticSlam.java
+++ src/main/java/com/josinosle/magicengines/content/spell/spellcontent/combat/TelekeneticSlam.java
@@ -17,24 +17,27 @@ public class TelekeneticSlam {
         AABB boundBox = new AABB(vector.getX() - 5, vector.getY() - 5, vector.getZ() - 5, vector.getX() + 5, vector.getY() + 5, vector.getZ() + 5);
         List<Entity> entToDamage = player.getLevel().getEntities(null, boundBox);
         for (Entity i : entToDamage) {
-            // change entity delta movement
-            i.setDeltaMovement(
-                    i.getX()-player.getX(),
-                    i.getY()-player.getY()+0.2,
-                    i.getZ()-player.getZ());
+            if(i.getId()!=player.getId()){
+
+                // change entity delta movement
+                i.setDeltaMovement(
+                        i.getX() - player.getX(),
+                        i.getY() - player.getY() + 0.2,
+                        i.getZ() - player.getZ());
 
 
                 // send spawn particle
-            player.getLevel().sendParticles(
-                    ParticleTypes.EXPLOSION,
-                    i.getX(),
-                    i.getY()+1,
-                    i.getZ(),
-                    1,
-                    0,
-                    0.05,
-                    0,
-                    0);
+                player.getLevel().sendParticles(
+                        ParticleTypes.EXPLOSION,
+                        i.getX(),
+                        i.getY() + 1,
+                        i.getZ(),
+                        1,
+                        0,
+                        0.05,
+                        0,
+                        0);
+            }
         }
     }
 }
