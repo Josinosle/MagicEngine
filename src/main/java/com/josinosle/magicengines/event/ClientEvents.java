ced93f2d6d04d3c4b47e9f15201861932110bb0a
florianhirson <29225021+florianhirson@users.noreply.github.com>
2024-03-14 16:32:43 +0100

    Move ClientModBusEvents to a dedicated class file

--- src/main/java/com/josinosle/magicengines/event/ClientEvents.java
+++ src/main/java/com/josinosle/magicengines/event/ClientEvents.java
@@ -1,7 +1,6 @@
 package com.josinosle.magicengines.event;
 
 import com.josinosle.magicengines.MagicEngines;
-import com.josinosle.magicengines.client.overlay.ManaBarOverlay;
 import com.josinosle.magicengines.content.item.MagicWand;
 import com.josinosle.magicengines.networking.Messages;
 import com.josinosle.magicengines.networking.packet.CalculateCastC2SPacket;
@@ -11,9 +10,6 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -38,23 +34,6 @@ public class ClientEvents {
             if(KeyboardHelper.CASTCALC_KEY.consumeClick() && localPlayer != null){
                 Messages.sendToServer(new CalculateCastC2SPacket());
             }
-        }
-
-        @Mod.EventBusSubscriber(modid = MagicEngines.MOD_ID, value = Dist.CLIENT, bus=Mod.EventBusSubscriber.Bus.MOD)
-        public class ClientModBusEvents {
-
-
-            @SubscribeEvent
-            public static void onKeyRegister(RegisterKeyMappingsEvent event){
-                event.register(KeyboardHelper.CAST_INAIR_KEY);
-                event.register(KeyboardHelper.CASTCALC_KEY);
-            }
-
-            @SubscribeEvent
-            public static void onRegisterOverlays(RegisterGuiOverlaysEvent event){
-                event.registerBelow(VanillaGuiOverlay.EXPERIENCE_BAR.id(), "mana_overlay", ManaBarOverlay::renderManaBar);
-            }
-
         }
     }
 }
---
+++ src/main/java/com/josinosle/magicengines/event/ClientModBusEvents.java
@@ -0,0 +1,28 @@
+package com.josinosle.magicengines.event;
+
+import com.josinosle.magicengines.MagicEngines;
+import com.josinosle.magicengines.client.overlay.ManaBarOverlay;
+import com.josinosle.magicengines.util.KeyboardHelper;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = MagicEngines.MOD_ID, value = Dist.CLIENT, bus=Mod.EventBusSubscriber.Bus.MOD)
+public class ClientModBusEvents {
+
+
+    @SubscribeEvent
+    public static void onKeyRegister(RegisterKeyMappingsEvent event){
+        event.register(KeyboardHelper.CAST_INAIR_KEY);
+        event.register(KeyboardHelper.CASTCALC_KEY);
+    }
+
+    @SubscribeEvent
+    public static void onRegisterOverlays(RegisterGuiOverlaysEvent event){
+        event.registerBelow(VanillaGuiOverlay.EXPERIENCE_BAR.id(), "mana_overlay", ManaBarOverlay::renderManaBar);
+    }
+
+}
