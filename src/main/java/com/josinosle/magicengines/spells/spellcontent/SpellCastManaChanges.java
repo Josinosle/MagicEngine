1815034ae96d768cf8641ebda84945055988ab6f
florianhirson <29225021+florianhirson@users.noreply.github.com>
2024-03-23 13:35:59 +0100

    Set unlimited spells for creative players

--- src/main/java/com/josinosle/magicengines/spells/spellcontent/SpellCastManaChanges.java
+++ src/main/java/com/josinosle/magicengines/spells/spellcontent/SpellCastManaChanges.java
@@ -9,14 +9,23 @@ public class SpellCastManaChanges {
 
     public boolean spellCastable(ServerPlayer player, int thresholdMana) {
         player.getCapability(PlayerManaProvider.PLAYER_MANA).ifPresent(mana -> {
-            if(mana.getMana()>=thresholdMana) {
-                this.isCastable = mana.getMana() >= thresholdMana;
+            if(!player.isCreative()) {
+                if(mana.getMana() >= thresholdMana) {
+                    this.isCastable = mana.getMana() >= thresholdMana;
+                }
+            } else {
+                this.isCastable = true;
             }
+
         });
         return isCastable;
     }
 
     public void subMana(ServerPlayer player, int sub){
-        player.getCapability(PlayerManaProvider.PLAYER_MANA).ifPresent(mana -> mana.subMana(sub));
+        player.getCapability(PlayerManaProvider.PLAYER_MANA).ifPresent(mana -> {
+            if(!player.isCreative()) {
+                mana.subMana(sub);
+            }
+        });
     }
 }
