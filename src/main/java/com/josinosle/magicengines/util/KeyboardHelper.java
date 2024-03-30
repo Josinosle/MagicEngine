b547c1bf6aff056900d404b99a23bc7a4541e919
Josinosle <josephbridger3@gmail.com>
2024-03-30 15:18:55 +0000

    changed default keybinds for casting

--- src/main/java/com/josinosle/magicengines/util/KeyboardHelper.java
+++ src/main/java/com/josinosle/magicengines/util/KeyboardHelper.java
@@ -13,11 +13,11 @@ public class KeyboardHelper {
     public static final String KEY_CAST_INAIR = "key.magicengine.cast_inair";
 
     public static final KeyMapping CAST_INAIR_KEY = new KeyMapping(KEY_CAST_INAIR, KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_CONTROL, KEY_CATEGORY_MAGIC_ENGINE);
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_O, KEY_CATEGORY_MAGIC_ENGINE);
 
     // cast calculation
     public static final String KEY_CAST_CASTCALC = "key.magicengine.cast_castcalc";
 
     public static final KeyMapping CASTCALC_KEY = new KeyMapping(KEY_CAST_CASTCALC, KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_SHIFT, KEY_CATEGORY_MAGIC_ENGINE);
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, KEY_CATEGORY_MAGIC_ENGINE);
 }
